c6a496089e411f877822180bc9e3d89a42bca0dc
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-11 19:11:11 +0530

    Setup Project and Setup Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import Explore from "./pages/Explore";
+import { BrowserRouter, Route } from "react-router-dom";
+import ProjectsPage from "./pages/ProjectsPage";
+import Project from "./components/Project";
+import { data } from "./assests/data";
+import Contact from "./pages/Contact";
+
+export const names = [];
 
 function App() {
+  data.forEach((each) => {
+    names.push("/" + each.name);
+  });
+
+  console.log("Names ", names);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact>
+        <LandingPage />
+      </Route>
+      <Route path="/explore" exact>
+        <Explore />
+      </Route>
+      <Route path="/project" exact>
+        <ProjectsPage />
+      </Route>
+      <Route path={names} exact>
+        <Project />
+      </Route>
+      <Route path="/contact" exact>
+        <Contact />
+      </Route>
+    </BrowserRouter>
   );
 }
 
