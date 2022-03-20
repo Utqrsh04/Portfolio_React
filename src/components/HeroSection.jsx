5ef2553bd2857f382770016d4939daa82d7afd21
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-03-20 19:08:22 +0530

    Added alt prop in img tag

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -38,15 +38,18 @@ const HeroSection = () => {
           <div className="mt-4 bg-primaryGray bg-opacity-30 rounded-md w-36 space-x-4 flex items-center justify-evenly">
             <img
               className="h-10"
+              alt="icons"
               src="https://img.icons8.com/plasticine/100/000000/instagram-new--v2.png"
             />
             <img
               className="w-8"
+              alt="icons"
               src="https://img.icons8.com/fluency-systems-regular/48/000000/github.png"
             />
 
             <img
               className="h-7 pr-1"
+              alt="icons"
               src="https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-linkedin-social-media-justicon-lineal-color-justicon.png"
             />
           </div>
